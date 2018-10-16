749d63a2082fc51820193681f2672122fe4456b9
Samsoedien <samsoedien@Samsoediens-MacBook-Pro.local>
2018-10-16 23:20:25 +0200

    add ESP8266 lib

--- ESP8266-data-collector.ino
+++ ESP8266-data-collector.ino
@@ -26,54 +26,54 @@
  * Date Modified: 17 Oct 2018
  */
 
+#include "ESP8266WiFi.h"       // Library to connect ESP8266 to WiFi network.
+#include "ESP8266HTTPClient.h" // Library to perform HTTP Requests.
+#include "ArduinoJson.h"       // Library to parse sensordata in JSON notation.
 
-#include <ESP8266WiFi.h>                  // Library to connect ESP8266 to WiFi network.
-#include <ESP8266HTTPClient.h>            // Library to perform HTTP Requests.
-#include <ArduinoJson.h>                  // Library to parse sensordata in JSON notation.
-
-#define ANALOG_PIN A0                     // Defined pin for the analog reading.              
-//#define ANALOG_PIN1 A1                  // Defined pin for the analog reading.              
-//#define ANALOG_PIN2 A2                  // Defined pin for the analog reading.              
-
+#define ANALOG_PIN A0 // Defined pin for the analog reading.
+//#define ANALOG_PIN1 A1                  // Defined pin for the analog reading.
+//#define ANALOG_PIN2 A2                  // Defined pin for the analog reading.
 
 // WiFi parameters to be configured
-const char* ssid = "Samsoedien iPhone";   // The SSID of the connected network.
-const char* password = "xagven4jwcdxx";   // The password of the connected network.
+const char *ssid = "Samsoedien iPhone"; // The SSID of the connected network.
+const char *password = "xagven4jwcdxx"; // The password of the connected network.
 
-const char* httpEndpoint = "http://beautiful-data.herokuapp.com/api/heartdata";   
+const char *httpEndpoint = "http://beautiful-data.herokuapp.com/api/heartdata";
 
 //const char* route = "/weatherdata";
 //const char* route = "/drowsydata";
-const char* route = "/stressdata";
+const char *route = "/stressdata";
 
 // Timer variables
 unsigned long previousMillis = 0;
 int intervalTimer = 10000;
- 
-void setup(void) { 
+
+void setup(void)
+{
   Serial.begin(115200);
-  Serial.println();  //Clear some garbage that may be printed to the serial console
+  Serial.println(); //Clear some garbage that may be printed to the serial console
   Serial.print("Connecting..");
   WiFi.begin(ssid, password); // Connect to WiFi
- 
+
   // while wifi not connected yet, print '.'
   // then after it connected, get out of the loop
-  while (WiFi.status() != WL_CONNECTED) {
-     delay(500);
-     Serial.print(".");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
   }
-  
+
   // Connected.
   Serial.println("Connection Successful!");
-  
+
   // Access Point (SSID).
   Serial.print("SSID: ");
   Serial.println(WiFi.SSID());
-  
+
   // IP adres.
   Serial.print("IP: ");
   Serial.println(WiFi.localIP());
- 
+
   // Signal Strength.
   long rssi = WiFi.RSSI();
   Serial.print("Signal Strength (RSSI): ");
@@ -81,33 +81,38 @@ void setup(void) {
   Serial.println(" dBm");
   Serial.println("");
 }
- 
-void loop() {
+
+void loop()
+{
   unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= intervalTimer) {
+  if (currentMillis - previousMillis >= intervalTimer)
+  {
     Serial.println("Posting Data..");
     heartData();
     Serial.println("heartValue");
     HTTPPostRequest(110);
     previousMillis = currentMillis;
   }
-} 
+}
 
-int heartData() {
+int heartData()
+{
   //int heartValue = analogRead(ANALOG_PIN);
   int heartValue = 80;
   return heartValue;
 }
 
-void HTTPPostRequest(int analogVal) {
-  if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
-    
-    StaticJsonBuffer<300> JSONbuffer;   //Declaring static JSON buffer
-    JsonObject& JSONencoder = JSONbuffer.createObject(); 
-    
-    JSONencoder["bpm"] = analogVal;             // specify your attribute name and the sensordata it needs to send.
+void HTTPPostRequest(int analogVal)
+{
+  if (WiFi.status() == WL_CONNECTED)
+  { //Check WiFi connection status
+
+    StaticJsonBuffer<300> JSONbuffer; //Declaring static JSON buffer
+    JsonObject &JSONencoder = JSONbuffer.createObject();
+
+    JSONencoder["bpm"] = analogVal; // specify your attribute name and the sensordata it needs to send.
     JSONencoder["emotion"] = "Hi";
- 
+
     /*
     JsonArray& values = JSONencoder.createNestedArray("values"); //JSON array
     values.add(20); //Add value to array
@@ -119,24 +124,26 @@ void HTTPPostRequest(int analogVal) {
     timestamps.add("10:20"); //Add value to array
     timestamps.add("10:30"); //Add value to array
     */
- 
+
     char JSONmessageBuffer[300];
     JSONencoder.prettyPrintTo(JSONmessageBuffer, sizeof(JSONmessageBuffer));
     Serial.println(JSONmessageBuffer);
- 
-    HTTPClient http;                                                              //Declare object of class HTTPClient
- 
-    http.begin(httpEndpoint);                                                     //Specify request destination
-    http.addHeader("Content-Type", "application/json");                           //Specify content-type header
- 
-    int httpCode = http.POST(JSONmessageBuffer);                                  //Send the request
-    String payload = http.getString();                                            //Get the response payload
- 
-    Serial.println(httpCode);                                                     //Print HTTP return code
-    Serial.println(payload);                                                      //Print request response payload
- 
-    http.end();                                                                   //Close connection
-  } else {
-    Serial.println("Error in WiFi connection"); 
+
+    HTTPClient http; //Declare object of class HTTPClient
+
+    http.begin(httpEndpoint);                           //Specify request destination
+    http.addHeader("Content-Type", "application/json"); //Specify content-type header
+
+    int httpCode = http.POST(JSONmessageBuffer); //Send the request
+    String payload = http.getString();           //Get the response payload
+
+    Serial.println(httpCode); //Print HTTP return code
+    Serial.println(payload);  //Print request response payload
+
+    http.end(); //Close connection
+  }
+  else
+  {
+    Serial.println("Error in WiFi connection");
   }
 }
---
+++ lib/ESP8266WiFi/examples/WiFiClient/WiFiClient.ino
@@ -0,0 +1,82 @@
+/*
+    This sketch establishes a TCP connection to a "quote of the day" service.
+    It sends a "hello" message, and then prints received data.
+*/
+
+#include <ESP8266WiFi.h>
+
+const char* ssid     = "your-ssid";
+const char* password = "your-password";
+
+const char* host = "djxmmx.net";
+const uint16_t port = 17;
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+
+  // We start by connecting to a WiFi network
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  /* Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
+     would try to act as both a client and an access-point and could cause
+     network-issues with your other WiFi-devices on your WiFi-network. */
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  Serial.print("connecting to ");
+  Serial.print(host);
+  Serial.print(':');
+  Serial.println(port);
+
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+  if (!client.connect(host, port)) {
+    Serial.println("connection failed");
+    delay(5000);
+    return;
+  }
+
+  // This will send a string to the server
+  Serial.println("sending data to server");
+  client.println("hello from ESP8266");
+  unsigned long timeout = millis();
+  while (client.available() == 0) {
+    if (millis() - timeout > 5000) {
+      Serial.println(">>> Client Timeout !");
+      client.stop();
+      delay(60000);
+      return;
+    }
+  }
+
+  // Read all the lines of the reply from server and print them to Serial
+  Serial.println("receiving from remote server");
+  while (client.available()) {
+    char ch = static_cast<char>(client.read());
+    Serial.print(ch);
+  }
+
+  // Close the connection
+  Serial.println();
+  Serial.println("closing connection");
+  client.stop();
+
+  delay(300000); // execute once every 5 minutes, don't flood remote service
+}
